a2a9a064e982d86424d549d60eef03788a4f109a
defjoy <defjoy9@users.noreply.github.com>
2025-09-09 14:15:57 +0200

    if you shoot an asteroid itll disappear

--- main.py
+++ main.py
@@ -43,6 +43,11 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    shot.kill()
+                    asteroid.kill()
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000 # 60 FPS
         
