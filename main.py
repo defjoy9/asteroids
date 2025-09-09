3fc9e46182704b5ed13f71d7db53a1245c2caded
defjoy <defjoy9@users.noreply.github.com>
2025-09-09 10:52:15 +0200

    there are now circles floating

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         # makes the exit button work
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+
         updatable.update(dt)
         screen.fill("black")
 
@@ -25,7 +34,7 @@ def main():
             obj.draw(screen)
 
         pygame.display.flip()
-        df = clock.tick(60) / 1000 # 60 FPS
+        dt = clock.tick(60) / 1000 # 60 FPS
         
 
 
